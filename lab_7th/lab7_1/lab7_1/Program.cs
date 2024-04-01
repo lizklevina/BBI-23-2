808b09d841ed0a515eb9f9f177bdcbf17f37135f
lizklevina <159522033+lizklevina@users.noreply.github.com>
2024-04-01 16:24:37 +0300

    lab_7

---
+++ lab_7th/lab7_1/lab7_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+abstract class Runner
+{
+    protected string lastName;
+    protected string group;
+    protected string teacherLastName;
+    protected double result;
+
+    public double Result => result;
+
+    public Runner(string lastName, string group, string teacherLastName, double result)
+    {
+        this.lastName = lastName;
+        this.group = group;
+        this.teacherLastName = teacherLastName;
+        this.result = result;
+    }
+
+    public abstract void Print();
+}
+
+class Runner100m : Runner
+{
+    public Runner100m(string lastName, string group, string teacherLastName, double result)
+        : base(lastName, group, teacherLastName, result)
+    {
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"Фамилия: {lastName} | Группа: {group} | Преподаватель: {teacherLastName} | Результат (100 м): {result}");
+    }
+}
+
+class Runner500m : Runner
+{
+    public Runner500m(string lastName, string group, string teacherLastName, double result)
+        : base(lastName, group, teacherLastName, result)
+    {
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"Фамилия: {lastName} | Группа: {group} | Преподаватель: {teacherLastName} | Результат (500 м): {result}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Runner[] participants = new Runner[5];
+        participants[0] = new Runner500m("Попова", "Группа 1", "Иванов", 210);
+        participants[1] = new Runner100m("Иванова", "Группа 2", "Петров", 180);
+        participants[2] = new Runner100m("Сидорова", "Группа 1", "Смирнов", 195);
+        participants[3] = new Runner500m("Петрова", "Группа 3", "Козлов", 220);
+        participants[4] = new Runner100m("Смирнова", "Группа 2", "Васильев", 190);
+
+        Array.Sort(participants, (x, y) => x.Result.CompareTo(y.Result));
+
+        Console.WriteLine("\nРезультаты забега:");
+        Console.WriteLine("=============================================");
+        Console.WriteLine("Фамилия\t\tГруппа\t\tПреподаватель\t\tРезультат");
+        Console.WriteLine("=============================================");
+
+        foreach (var participant in participants)
+        {
+            participant.Print();
+        }
+
+        Console.WriteLine("=============================================");
+    }
+}
---
+++ lab_7th/lab7_2/lab7_2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+abstract class Discipline
+{
+    protected string disciplineName;
+
+    public Discipline(string name)
+    {
+        disciplineName = name;
+    }
+
+    public abstract void PrintHeader();
+}
+
+class LongJump : Discipline
+{
+    public LongJump(string name) : base(name) { }
+
+    public override void PrintHeader()
+    {
+        Console.WriteLine($"\nПротокол соревнований по {disciplineName}:");
+        Console.WriteLine("=============================================");
+        Console.WriteLine("Фамилия\t\tЛучший результат");
+        Console.WriteLine("=============================================");
+    }
+}
+
+class HighJump : Discipline
+{
+    public HighJump(string name) : base(name) { }
+
+    public override void PrintHeader()
+    {
+        Console.WriteLine($"\nПротокол соревнований по {disciplineName}:");
+        Console.WriteLine("=============================================");
+        Console.WriteLine("Фамилия\t\tЛучший результат");
+        Console.WriteLine("=============================================");
+    }
+}
+
+class Participant
+{
+    private string lastName;
+    private double[] results;
+
+    public string LastName => lastName;
+    public double[] Results => results;
+
+    public Participant(string lastName, double[] results)
+    {
+        this.lastName = lastName;
+        this.results = results;
+    }
+
+    public double BestResult()
+    {
+        double bestResult = results[0];
+        for (int i = 1; i < results.Length; i++)
+        {
+            if (results[i] > bestResult)
+            {
+                bestResult = results[i];
+            }
+        }
+        return bestResult;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Фамилия: {LastName} | Лучший результат: {BestResult()}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Discipline longJump = new LongJump("прыжкам в длину");
+        Discipline highJump = new HighJump("прыжкам в высоту");
+
+        Participant[] participants = new Participant[5];
+        participants[0] = new Participant("Попов", new double[] { 4.5, 4.6, 4.7 });
+        participants[1] = new Participant("Иванов", new double[] { 4.8, 4.9, 5.0 });
+        participants[2] = new Participant("Сидоров", new double[] { 5.1, 5.2, 5.3 });
+        participants[3] = new Participant("Петров", new double[] { 5.4, 5.5, 5.6 });
+        participants[4] = new Participant("Смирнов", new double[] { 5.7, 5.8, 5.9 });
+
+        longJump.PrintHeader();
+        foreach (var participant in participants)
+        {
+            participant.Print();
+        }
+        Console.WriteLine("=============================================");
+
+        highJump.PrintHeader();
+        foreach (var participant in participants)
+        {
+            participant.Print();
+        }
+        Console.WriteLine("=============================================");
+    }
+}
