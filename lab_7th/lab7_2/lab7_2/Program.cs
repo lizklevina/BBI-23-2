ced7adc82a156470a2c8d6bfebc31ba431f5c666
lizklevina <159522033+lizklevina@users.noreply.github.com>
2024-04-06 13:07:14 +0300

    внесла поправки по 2 номеру

--- lab_7th/lab7_2/lab7_2/Program.cs
+++ lab_7th/lab7_2/lab7_2/Program.cs
@@ -1,20 +1,47 @@
-﻿using System;
+using System;
 
 abstract class Discipline
 {
     protected string disciplineName;
+    protected Participant[] participants;
+    protected int participantCount;
 
-    public Discipline(string name)
+    public Discipline(string name, int maxParticipants)
     {
         disciplineName = name;
+        participants = new Participant[maxParticipants];
+        participantCount = 0;
+    }
+
+    public void AddParticipant(Participant participant)
+    {
+        if (participantCount < participants.Length)
+        {
+            participants[participantCount] = participant;
+            participantCount++;
+        }
+        else
+        {
+            Console.WriteLine("Достигнуто максимальное количество участников.");
+        }
     }
 
     public abstract void PrintHeader();
+
+    public void PrintParticipants()
+    {
+        foreach (var participant in participants)
+        {
+            if (participant != null)
+                participant.Print();
+        }
+        Console.WriteLine("=============================================");
+    }
 }
 
 class LongJump : Discipline
 {
-    public LongJump(string name) : base(name) { }
+    public LongJump(string name, int maxParticipants) : base(name, maxParticipants) { }
 
     public override void PrintHeader()
     {
@@ -27,7 +54,7 @@ class LongJump : Discipline
 
 class HighJump : Discipline
 {
-    public HighJump(string name) : base(name) { }
+    public HighJump(string name, int maxParticipants) : base(name, maxParticipants) { }
 
     public override void PrintHeader()
     {
@@ -75,28 +102,25 @@ class Program
 {
     static void Main()
     {
-        Discipline longJump = new LongJump("прыжкам в длину");
-        Discipline highJump = new HighJump("прыжкам в высоту");
+        LongJump longJump = new LongJump("прыжкам в длину", 5);
+        HighJump highJump = new HighJump("прыжкам в высоту", 5);
 
-        Participant[] participants = new Participant[5];
-        participants[0] = new Participant("Попов", new double[] { 4.5, 4.6, 4.7 });
-        participants[1] = new Participant("Иванов", new double[] { 4.8, 4.9, 5.0 });
-        participants[2] = new Participant("Сидоров", new double[] { 5.1, 5.2, 5.3 });
-        participants[3] = new Participant("Петров", new double[] { 5.4, 5.5, 5.6 });
-        participants[4] = new Participant("Смирнов", new double[] { 5.7, 5.8, 5.9 });
+        longJump.AddParticipant(new Participant("Попов", new double[] { 4.5, 4.6, 4.7 }));
+        longJump.AddParticipant(new Participant("Иванов", new double[] { 4.8, 4.9, 5.0 }));
+        longJump.AddParticipant(new Participant("Сидоров", new double[] { 5.1, 5.2, 5.3 }));
+        longJump.AddParticipant(new Participant("Петров", new double[] { 5.4, 5.5, 5.6 }));
+        longJump.AddParticipant(new Participant("Смирнов", new double[] { 5.7, 5.8, 5.9 }));
+
+        highJump.AddParticipant(new Participant("Попов", new double[] { 1.5, 1.6, 1.7 }));
+        highJump.AddParticipant(new Participant("Иванов", new double[] { 1.8, 1.9, 2.0 }));
+        highJump.AddParticipant(new Participant("Сидоров", new double[] { 2.1, 2.2, 2.3 }));
+        highJump.AddParticipant(new Participant("Петров", new double[] { 2.4, 2.5, 2.6 }));
+        highJump.AddParticipant(new Participant("Смирнов", new double[] { 2.7, 2.8, 2.9 }));
 
         longJump.PrintHeader();
-        foreach (var participant in participants)
-        {
-            participant.Print();
-        }
-        Console.WriteLine("=============================================");
+        longJump.PrintParticipants();
 
         highJump.PrintHeader();
-        foreach (var participant in participants)
-        {
-            participant.Print();
-        }
-        Console.WriteLine("=============================================");
+        highJump.PrintParticipants();
     }
 }
