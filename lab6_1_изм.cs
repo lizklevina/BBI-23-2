6deecd214c7acd4db6a404b55a659374a8f8042e
lizklevina <159522033+lizklevina@users.noreply.github.com>
2024-04-19 12:19:02 +0300

    Add files via upload

---
+++ lab6_1_изм.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+struct Ychastniki
+{
+    private string lastName;
+    private string group;
+    private string teacherLastName;
+    private double result;
+
+    public string LastName => lastName;
+    public string Group => group;
+    public string TeacherLastName => teacherLastName;
+    public double Result => result;
+
+    public Ychastniki(string lastName, string group, string teacherLastName, double result)
+    {
+        this.lastName = lastName;
+        this.group = group;
+        this.teacherLastName = teacherLastName;
+        this.result = result;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Фамилия: {LastName} | Группа: {Group} | Преподаватель: {TeacherLastName} | Результат: {Result}");
+    }
+}
+
+class Program
+{
+    static void Sort(Ychastniki[] participants)
+    {
+        int i = 1;
+        int j = i + 1;
+        while (i < participants.Length)
+        {
+            if (i == 0 || participants[i].Result >= participants[i - 1].Result)
+            {
+                i = j;
+                j = i + 1;
+            }
+            else if (participants[i].Result < participants[i - 1].Result)
+            {
+                (participants[i - 1], participants[i]) = (participants[i], participants[i - 1]);
+                if (i > 1)
+                    i--;
+            }
+        }
+    }
+
+
+
+    static void Main()
+    {
+        Ychastniki[] participants = new Ychastniki[5];
+        participants[0] = new Ychastniki("Попова", "Группа 1", "Иванов", 210);
+        participants[1] = new Ychastniki("Иванова", "Группа 2", "Петров", 180);
+        participants[2] = new Ychastniki("Сидорова", "Группа 1", "Смирнов", 195);
+        participants[3] = new Ychastniki("Петрова", "Группа 3", "Козлов", 220);
+        participants[4] = new Ychastniki("Смирнова", "Группа 2", "Васильев", 190);
+
+        Sort(participants);
+
+        Console.WriteLine("\nРезультаты кросса на 500 м для женщин:");
+        Console.WriteLine("=============================================");
+        Console.WriteLine("Фамилия\t\tГруппа\t\tПреподаватель\t\tРезультат");
+        Console.WriteLine("=============================================");
+
+        foreach (var participant in participants)
+        {
+            participant.Print();
+        }
+
+        Console.WriteLine("=============================================");
+    }
+}
---
+++ lab6_3_изм.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+struct PollQuestion
+{
+    private string question;
+    private Dictionary<string, int> responses;
+    private int totalVotes; 
+
+    public string Question => question;
+    public Dictionary<string, int> Responses => responses;
+    public int TotalVotes => totalVotes; 
+
+    public PollQuestion(string question)
+    {
+        this.question = question;
+        responses = new Dictionary<string, int>();
+        totalVotes = 0; 
+    }
+
+    public void AddVote(string response)
+    {
+        if (responses.ContainsKey(response))
+            responses[response]++;
+        else
+            responses[response] = 1;
+
+        totalVotes++; 
+    }
+}
+
+class RadioPoll
+{
+    static void Main()
+    {
+        PollQuestion animalPoll = new PollQuestion("Какое животное Вы связываете с Японией и японцами?");
+        PollQuestion characterTraitPoll = new PollQuestion("Какую черту характера Вы связываете с Японией и японцами?");
+        PollQuestion objectPoll = new PollQuestion("Какой неодушевленный предмет Вы связываете с Японией и японцами?");
+
+        FillResponses(animalPoll, animalPoll.Question);
+        FillResponses(characterTraitPoll, characterTraitPoll.Question);
+        FillResponses(objectPoll, objectPoll.Question);
+
+        PrintTopResponses(animalPoll.Responses, animalPoll.Question, animalPoll.TotalVotes);
+        PrintTopResponses(characterTraitPoll.Responses, characterTraitPoll.Question, characterTraitPoll.TotalVotes);
+        PrintTopResponses(objectPoll.Responses, objectPoll.Question, objectPoll.TotalVotes);
+    }
+
+    static void FillResponses(PollQuestion pollQuestion, string question)
+    {
+        Console.WriteLine($"Введите ответы на вопрос: {question} (для завершения введите 'done')");
+
+        while (true)
+        {
+            Console.Write("Ответ: ");
+            string answer = Console.ReadLine().Trim().ToLower();
+
+            if (answer == "done")
+                break;
+
+            if (string.IsNullOrEmpty(answer))
+            {
+                Console.WriteLine("Ответ не может быть пустым. Повторите ввод.");
+                continue;
+            }
+
+            pollQuestion.AddVote(answer);
+        }
+    }
+
+    static void PrintTopResponses(Dictionary<string, int> responses, string question, int totalVotes)
+    {
+        Console.WriteLine($"\nТоп-5 наиболее часто встречающихся ответов на вопрос: {question}");
+
+        if (responses.Count == 0)
+        {
+            Console.WriteLine("Ответы отсутствуют.");
+            return;
+        }
+
+        var sortedResponses = responses.OrderByDescending(x => x.Value).Take(5);
+
+        foreach (var response in sortedResponses)
+        {
+            double percentage = (double)response.Value / totalVotes * 100;
+            Console.WriteLine($"{response.Key}: {response.Value} ({percentage:f2}%)");
+        }
+    }
+}
